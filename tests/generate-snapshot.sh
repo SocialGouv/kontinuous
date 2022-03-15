0c67ab5592637cc9cc06682afdaa9ef938e0ca58
devthejo <jo@surikat.pro>
2022-03-15 02:29:10 +0100

    feat(tests): multiple folders

--- tests/generate-snapshot.sh
+++ tests/generate-snapshot.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 set -e
 
-export WORKSPACE_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-export KUBEWORKFLOW_PATH=$(dirname $WORKSPACE_PATH)
+export ROOT_WORKSPACE_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+export KUBEWORKFLOW_PATH=$(dirname $ROOT_WORKSPACE_PATH)
 
 export AUTODEVOPS_PATH="/tmp/autodevops"
 
-$KUBEWORKFLOW_PATH/dev-local.sh
+shopt -s globstar
+for dir in $ROOT_WORKSPACE_PATH/*; do
+  [ -d "$dir" ] || continue
+  export WORKSPACE_PATH="$dir"
+  if [ -f "$WORKSPACE_PATH/.env" ]; then
+    source "$WORKSPACE_PATH/.env"
+  fi
+  $KUBEWORKFLOW_PATH/dev-local.sh
+  cp $AUTODEVOPS_PATH/manifests.yaml $WORKSPACE_PATH/manifests.yaml.snap
+done
 
-cp $AUTODEVOPS_PATH/manifests.yaml $WORKSPACE_PATH/manifests.yaml.snap
-
-echo "Snapshot: $WORKSPACE_PATH/manifests.yaml.snap"
+for dir in $ROOT_WORKSPACE_PATH/*; do
+  [ -f "$dir/manifests.yaml.snap" ] || continue
+  echo "Snapshot: $dir/manifests.yaml.snap"
+done
