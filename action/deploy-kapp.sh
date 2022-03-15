e1c08020446dbab9efebbab39b5d46f345ed02ba
devthejo <jo@surikat.pro>
2022-03-15 13:19:18 +0100

    fix(wip): path

--- action/deploy-kapp.sh
+++ action/deploy-kapp.sh
@@ -3,12 +3,14 @@
 export $($(dirname $0)/env.sh)
 
 export REPOSITORY_NAME=${REPOSITORY##*/}
+export NAMESPACE=$(cat manifests.yaml | yq eval-all 'select(.kind=="Namespace").metadata.name')
 
-$(dirname $0)/utils/need-vars.sh "REPOSITORY_NAME"
+$(dirname $0)/utils/need-vars.sh "REPOSITORY_NAME NAMESPACE"
 
 kapp \
   deploy \
   --app $REPOSITORY_NAME \
+  --namespace $NAMESPACE \
   --logs-all \
   --yes \
   -f manifests.yaml
