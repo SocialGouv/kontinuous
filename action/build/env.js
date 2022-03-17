a0a223425db2127d3c35c5906e691fb59d88dc23
devthejo <jo@surikat.pro>
2022-03-17 18:51:33 +0100

    fix: yarn

--- action/build/env.js
+++ action/build/env.js
@@ -4,10 +4,10 @@ const envsDir = `${__dirname}/envs`
 const getFiles = (path) => fs.readdirSync(path).filter(file => fs.lstatSync(`${path}/${file}`).isFile())
 const envs = getFiles(envsDir)
 
-module.export = ()=>{
+module.exports = () => {
   const env = {...process.env}
   for (const envFile of envs) {
-    const vars = require(envFile)
+    const vars = require(`${envsDir}/${envFile}`)
     for (const [dest, src] of Object.entries(vars)) {
       env[dest] = env[dest] || process.env[src]
     }
