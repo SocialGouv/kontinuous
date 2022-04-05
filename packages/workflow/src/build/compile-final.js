588308da7c5202314f382ce8b19bb3f970c0fdcc
devthejo <jo@surikat.pro>
2022-04-06 00:05:20 +0200

    fix: duplicates resources

--- packages/workflow/src/build/compile-final.js
+++ packages/workflow/src/build/compile-final.js
@@ -6,6 +6,9 @@ module.exports = async (manifestsDocument, values) => {
   const defaultNamespace = values.global.namespace
   const iterator = yaml.loadAll(manifestsDocument)
   const manifests = []
+
+  const uniqNames = new Set()
+
   for (const manifest of iterator) {
     if (!manifest) {
       continue
@@ -18,7 +21,17 @@ module.exports = async (manifestsDocument, values) => {
         manifest.metadata.namespace = defaultNamespace
       }
     }
+
+    if (manifest.metadata?.name) {
+      const n = `${manifest.kind}.${manifest.metadata.namespace}.${manifest.metadata.name}`
+      if (uniqNames.has(n)) {
+        throw new Error(`Duplicate ressource for ${manifest.metadata.name}`)
+      }
+      uniqNames.add(n)
+    }
+
     removeNulls(manifest)
+
     manifests.push(yaml.dump(manifest))
   }
   return manifests.join("---\n")
