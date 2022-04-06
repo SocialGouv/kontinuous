5578bd1e39fa43e3cbf0966e5168e79dd299ef74
devthejo <jo@surikat.pro>
2022-04-06 18:31:46 +0200

    chore: webhook wip

--- packages/webhook/src/api/v1/security/github-webhook-token.js
+++ packages/webhook/src/api/v1/security/github-webhook-token.js
@@ -1,6 +1,10 @@
 module.exports = ({ services: { verifyHmac } }) => {
   return async (req, _scopes, _schema) => {
     const signature = req.headers["X-Hub-Signature-256"]
-    return verifyHmac(signature, req.body)
+    return verifyHmac({
+      signature,
+      body: req.rawBody,
+      secret: process.env.KUBEWEBHOOK_TOKEN,
+    })
   }
 }
--- packages/webhook/src/api/v1/services/verify-hmac.js
+++ packages/webhook/src/api/v1/services/verify-hmac.js
@@ -1,8 +1,11 @@
 // const { ctx } = require("@modjo-plugins/core")
 // const { reqCtx } = require("@modjo-plugins/express/ctx")
+const crypto = require("crypto")
 
 module.exports = function () {
-  return (_signature, _body) => {
-    return true
+  return ({ signature, body, secret }) => {
+    const hmac = crypto.createHmac("sha1", secret)
+    hmac.update(body, "utf-8")
+    return signature === `sha1=${hmac.digest("hex")}`
   }
 }
