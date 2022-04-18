86fb0d677934b7629b795435e24665bd9481a8ae
devthejo <jo@surikat.pro>
2022-04-18 13:27:29 +0200

    chore: debug

--- packages/webhook/src/api/v1/services/verify-hmac.js
+++ packages/webhook/src/api/v1/services/verify-hmac.js
@@ -6,11 +6,9 @@ module.exports = function () {
   return ({ signature, body, secret, sigHashAlg = "sha256" }) => {
     const sig = Buffer.from(signature || "", "utf8")
     const hmac = crypto.createHmac(sigHashAlg, secret)
-    const digest = Buffer.from(
-      `${sigHashAlg}=${hmac.update(body).digest("hex")}`,
-      "utf8"
-    )
-    console.log(`${sigHashAlg}=${hmac.update(body).digest("hex")}`)
+    const hex = `${sigHashAlg}=${hmac.update(body).digest("hex")}`
+    const digest = Buffer.from(hex, "utf8")
+    console.log(hex)
     console.log("signature", signature)
     return sig.length === digest.length && crypto.timingSafeEqual(digest, sig)
   }
