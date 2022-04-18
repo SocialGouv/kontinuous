9c445f4a74aba0b6afc0989335a0fd0db2b496d6
devthejo <jo@surikat.pro>
2022-04-18 12:26:07 +0200

    fix: webhook signature

--- packages/webhook/src/api/v1/services/verify-hmac.js
+++ packages/webhook/src/api/v1/services/verify-hmac.js
@@ -3,9 +3,13 @@
 const crypto = require("crypto")
 
 module.exports = function () {
-  return ({ signature, body, secret }) => {
-    const hmac = crypto.createHmac("sha256", secret)
-    hmac.update(body, "utf-8")
-    return signature === `sha256=${hmac.digest("hex")}`
+  return ({ signature, body, secret, sigHashAlg = "sha256" }) => {
+    const sig = Buffer.from(signature || "", "utf8")
+    const hmac = crypto.createHmac(sigHashAlg, secret)
+    const digest = Buffer.from(
+      `${sigHashAlg}=${hmac.update(body).digest("hex")}`,
+      "utf8"
+    )
+    return sig.length === digest.length && crypto.timingSafeEqual(digest, sig)
   }
 }
