8fb05b3a68203abdc60dafee6354582006f2780d
devthejo <jo@surikat.pro>
2022-04-23 01:59:53 +0200

    fix: webhook ncc compilation

--- packages/webhook/src/daemon.js
+++ packages/webhook/src/daemon.js
@@ -2,6 +2,10 @@ const cron = require("node-cron")
 const cleanArtifact = require("~/cron/clean-artifact")
 
 module.exports = () => {
+  if (process.argv[2] === "build") {
+    return
+  }
+
   // at minute 0 past every 6th hour. see https://crontab.guru/
   cron.schedule("0 */6 * * *", cleanArtifact)
 }
