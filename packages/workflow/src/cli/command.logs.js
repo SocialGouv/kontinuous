59a5d9f647f90d7f5e39109738d94b9311bd34d7
devthejo <jo@surikat.pro>
2022-04-27 01:33:12 +0200

    fix: cli logs

--- packages/workflow/src/cli/command.logs.js
+++ packages/workflow/src/cli/command.logs.js
@@ -13,14 +13,17 @@ module.exports = program
   .addOption(options.cwd)
   .addOption(options.rancherProjectName)
   .addOption(options.webhookBaseDomain)
-  .option("--repository", "git repository url, default to current")
-  .option("--branch", "git branch, default to current")
-  .option("--commit", "git commit sha, default to last commit of branch")
-  .option("--token", "kubewebook token")
-  .option("--webhook-uri", "kubewebook uri")
+  .option("--repository <repo>", "git repository url, default to current")
+  .option("--branch <ref>", "git branch, default to current")
+  .option(
+    "--commit <commit>",
+    "git commit sha, default to last commit of branch"
+  )
+  .option("--token <token>", "kubewebook token")
+  .option("--webhook-uri <uri>", "kubewebook uri")
   .addOption(
     new Option(
-      "[event]",
+      "event",
       `pipeline event, default to "created" on version tag, "pushed" else`
     ).choices(["pushed", "deleted", "created"])
   )
