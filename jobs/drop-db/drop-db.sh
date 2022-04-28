43d540e4b359b1cc197cf339e036de99bb3f2fbd
devthejo <jo@surikat.pro>
2022-04-28 19:22:55 +0200

    fix: job drop db

---
+++ jobs/drop-db/drop-db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# check mandatory environment variables
+MANDATORY_VARS="PGPASSWORD PGHOST PGUSER DATABASE"
+for VAR in $MANDATORY_VARS; do
+  if [[ -z "${!VAR}" ]]; then
+    echo "${VAR} environment variable is empty"
+    exit 1
+  fi
+done
+
+PGPORT=${PGPORT:-5432}
+
+psql -c "DROP DATABASE \"${DATABASE}\";"
