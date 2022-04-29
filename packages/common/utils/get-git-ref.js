36dc904d74e999c32b05b76b78246a65cc9e539a
devthejo <jo@surikat.pro>
2022-04-29 14:44:02 +0200

    fix: git ref

--- packages/common/utils/get-git-ref.js
+++ packages/common/utils/get-git-ref.js
@@ -1,4 +1,9 @@
 const asyncShell = require("./async-shell")
 
-module.exports = async (cwd = process.cwd()) =>
-  (await asyncShell("git name-rev --name-only HEAD", { cwd })).trim()
+module.exports = async (cwd = process.cwd()) => {
+  let ref = (await asyncShell("git branch --show-current", { cwd })).trim()
+  if (!ref) {
+    ref = (await asyncShell("git name-rev --name-only HEAD", { cwd })).trim()
+  }
+  return ref
+}
