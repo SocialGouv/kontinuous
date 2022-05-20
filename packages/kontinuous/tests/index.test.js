30cf4f63a244dfd07d6024c6e27b3a313cf249ff
devthejo <jo@surikat.pro>
2022-05-20 16:50:41 +0200

    refactor: ultra big wip

--- packages/kontinuous/tests/index.test.js
+++ packages/kontinuous/tests/index.test.js
@@ -8,12 +8,11 @@ const cli = require("~/cli")
 
 const ctx = require("~/ctx")
 
-const rootPath = path.resolve(`${__dirname}/..`)
-
 const samplesDir = `${__dirname}/samples`
 const testdirs = getDirectoriesSync(samplesDir)
 
 const defaultEnv = {
+  KS_KONTINUOUS_PATH: path.resolve(`${__dirname}/..`),
   KS_GIT_REF: "refs/heads/feature-branch-1",
   KS_GIT_SHA: "ffac537e6cbbf934b08745a378932722df287a53",
   KS_ENVIRONMENT: "dev",
@@ -42,13 +41,12 @@ for (const testdir of testdirs) {
   }
 }
 
-test.each(cases)(`%s.%s`, async (testdir, environment) => {
+const createSampleSnapTest = (testdir, environment) => async () => {
   const testdirPath = `${samplesDir}/${testdir}`
   const env = {
     ...process.env,
     ...defaultEnv,
     KS_ENVIRONMENT: environment,
-    KS_KONTINUOUS_PATH: rootPath,
     KS_WORKSPACE_PATH: testdirPath,
     KS_WORKSPACE_SUBPATH: (await fs.pathExists(`${testdirPath}/.kontinuous`))
       ? "/.kontinuous"
@@ -70,4 +68,8 @@ test.each(cases)(`%s.%s`, async (testdir, environment) => {
   expect(manifests).toMatchSpecificSnapshot(
     `./__snapshots__/${testdir}.${environment}.yaml`
   )
-})
+}
+
+for (const [testdir, environment] of cases) {
+  test(`${testdir}.${environment}`, createSampleSnapTest(testdir, environment))
+}
