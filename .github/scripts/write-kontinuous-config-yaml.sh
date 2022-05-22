a73e2e8e8e86f032862173176a68051d55e41a18
devthejo <jo@surikat.pro>
2022-05-22 11:06:52 +0200

    fix: wip

--- .github/scripts/write-kontinuous-config-yaml.sh
+++ .github/scripts/write-kontinuous-config-yaml.sh
@@ -4,7 +4,7 @@ mkdir -p $HOME/.kontinuous
 
 echo "
 links:
-  SocialGouv/kontinuous: ${GITHUB_ACTION_PATH}/kontinuous
-  SocialGouv/kontinuous/plugins/recommended: ${GITHUB_ACTION_PATH}/plugins/recommended
-  SocialGouv/kontinuous/plugins/fabrique: ${GITHUB_ACTION_PATH}/plugins/fabrique
+  SocialGouv/kontinuous: ${GITHUB_WORKSPACE}/kontinuous
+  SocialGouv/kontinuous/plugins/recommended: ${GITHUB_WORKSPACE}/plugins/recommended
+  SocialGouv/kontinuous/plugins/fabrique: ${GITHUB_WORKSPACE}/plugins/fabrique
 " > $HOME/.kontinuous/config.yaml
