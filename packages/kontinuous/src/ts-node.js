59ad4540a033168943f7413dc6f36021ed8ec2dd
devthejo <jo@surikat.pro>
2022-06-27 20:03:30 +0200

    fix: ts-node

--- packages/kontinuous/src/ts-node.js
+++ packages/kontinuous/src/ts-node.js
@@ -1,7 +1,8 @@
 const path = require("path")
-const { register } = require("ts-node")
 
 process.env.TS_NODE_PROJECT = path.resolve(`${__dirname}/../tsconfig.json`)
+
+const { register } = require("ts-node")
 
 register({
   transpileOnly: true,
