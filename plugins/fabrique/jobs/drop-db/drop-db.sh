e085fe6448310d02299fc15bf51acbcfd741c584
devthejo <jo@surikat.pro>
2022-07-09 10:36:17 +0200

    fix: job drop-db improve

--- plugins/fabrique/jobs/drop-db/drop-db.sh
+++ plugins/fabrique/jobs/drop-db/drop-db.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # check mandatory environment variables
 MANDATORY_VARS="PGPASSWORD PGHOST PGUSER DATABASE"
@@ -11,4 +12,36 @@ done
 
 PGPORT=${PGPORT:-5432}
 
-dropdb "$DATABASE" || true
+# arobase replacement
+PG_URL_ADMIN="postgresql://${PGUSER/@/%40}:${PGPASSWORD}@${PGHOST}:${PGPORT}/postgres"
+DROP_USER_BASE=${PGUSER%%@*} || $PGUSER
+
+echo
+echo "disconnect activities on database ${DATABASE} on ${PGHOST}"
+psql -abe "$PG_URL_ADMIN" -c "
+  SET SESSION CHARACTERISTICS AS TRANSACTION READ WRITE;
+  -- Disconnect users from database
+  SELECT pg_terminate_backend (pg_stat_activity.pid)
+  FROM pg_stat_activity
+  WHERE pg_stat_activity.datname = '${DATABASE}';
+"
+
+echo
+echo "deleting database ${DATABASE} on ${PGHOST}"
+# dropdb "$DATABASE"
+psql -abe "$PG_URL_ADMIN" <<EOF
+  SET SESSION CHARACTERISTICS AS TRANSACTION READ WRITE;
+  DROP DATABASE "$DATABASE";
+EOF
+
+echo
+echo "deleting user ${DROP_USER_BASE} on ${PGHOST}"
+psql -abe "$PG_URL_ADMIN" <<EOF
+  SET SESSION CHARACTERISTICS AS TRANSACTION READ WRITE;
+  REVOKE ALL PRIVILEGES ON SCHEMA public FROM "$DROP_USER_BASE";
+  REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM "$DROP_USER_BASE";
+  REVOKE ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public FROM "$DROP_USER_BASE";
+  REVOKE ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public FROM "$DROP_USER_BASE";
+  DROP OWNED BY "${DROP_USER_BASE}" CASCADE;
+  DROP USER "${DROP_USER_BASE}";
+EOF
