7416ced71aec490c55e592efcc15a4e76a966ac7
devthejo <jo@surikat.pro>
2022-07-09 10:26:48 +0200

    feat: add KS_LOG_LEVEL

--- packages/common/utils/logger-factory.js
+++ packages/common/utils/logger-factory.js
@@ -15,11 +15,27 @@ module.exports = (options = {}) => {
   )
 
   const configureDebug = (debug) => {
-    if (debug && debug !== "0" && debug !== "false") {
+    if (
+      debug &&
+      debug !== "0" &&
+      debug !== "false" &&
+      pino.levels.values.debug < pino.levels.values[logger.level]
+    ) {
       logger.level = pino.levels.values.debug
     }
   }
 
+  if (process.env.KS_LOG_LEVEL) {
+    const logLevel = process.env.KS_LOG_LEVEL
+    const logLevels = Object.keys(pino.levels.values)
+    if (!logLevels.includes(logLevel)) {
+      throw new Error(
+        `unkown logLevel "${logLevel}", expected one of: ${logLevels.join(",")}`
+      )
+    }
+    const levelValue = pino.levels.values[logLevel]
+    logger.level = levelValue
+  }
   configureDebug(process.env.KS_DEBUG || process.env.DEBUG)
 
   logger.configureDebug = configureDebug
