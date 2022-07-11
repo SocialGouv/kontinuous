acb7cc32b20b054592c8966cd760b499068eab8a
devthejo <jo@surikat.pro>
2022-07-11 19:22:37 +0200

    fix: replace buggy git-url-parse

--- packages/common/utils/repository-from-git-url.js
+++ packages/common/utils/repository-from-git-url.js
@@ -1,13 +1,7 @@
-const GitUrlParse = require("git-url-parse")
+const parseGitUrl = require("parse-github-url")
 
 module.exports = (gitUrl) => {
-  const url = GitUrlParse(gitUrl)
-  let { pathname } = url
-  if (pathname.startsWith("/")) {
-    pathname = pathname.slice(1)
-  }
-  if (pathname.endsWith(".git")) {
-    pathname = pathname.slice(0, pathname.length - 4)
-  }
-  return pathname
+  const url = parseGitUrl(gitUrl)
+  const { repository } = url
+  return repository
 }
