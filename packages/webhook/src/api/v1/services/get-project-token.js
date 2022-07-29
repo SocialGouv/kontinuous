12487f9d7dd148bded23ec85d1ef31b6f1bbd716
devthejo <jo@surikat.pro>
2022-07-30 00:20:30 +0200

    fix: destructure

--- packages/webhook/src/api/v1/services/get-project-token.js
+++ packages/webhook/src/api/v1/services/get-project-token.js
@@ -1,9 +1,9 @@
 const { ctx } = require("@modjo-plugins/core")
 
 module.exports =
-  ({ services: getProject }) =>
+  ({ services }) =>
   (req) => {
-    const project = getProject(req)
+    const project = services.getProject(req)
     const tokens = ctx.require("config.project.secrets.tokens")
     const supertoken = ctx.require("config.project.secrets.supertoken")
     return [...(tokens[project] || []), supertoken]
