0b00f4eed064c10eb9d6c86c33aaefcf0929e26c
devthejo <jo@surikat.pro>
2022-08-15 17:47:39 +0200

    fix: normalize repo url

--- packages/common/utils/normalize-repository-url.js
+++ packages/common/utils/normalize-repository-url.js
@@ -2,9 +2,9 @@ const parseGitUrl = require("parse-github-url")
 
 module.exports = (repositoryUrl, protocol = "https") => {
   const url = parseGitUrl(repositoryUrl)
-  const auth = url.auth ? `${url.auth}@` : ""
+  const auth = url.auth ? `${url.auth}@` : protocol === "ssh" ? "git@" : ""
   const port = url.port ? `:${url.port}` : ""
-  const separator = protocol === "ssh" ? ":" : "/"
+  const separator = protocol === "git" ? ":" : "/"
   const repoUrl = `${protocol}://${auth}${
     url.hostname || "github.com"
   }${port}${separator}${url.repo}`
