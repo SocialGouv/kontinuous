020b0a616ca68c38eaf91b68f6fc1195dfb247f8
devthejo <jo@surikat.pro>
2022-08-16 18:34:19 +0200

    fix: add projectName to values

--- plugins/fabrique/values-compilers/global-defaults.js
+++ plugins/fabrique/values-compilers/global-defaults.js
@@ -106,6 +106,7 @@ module.exports = async (values, _options, { config, utils, ctx }) => {
 
   const defaultValues = {
     global: {
+      projectName,
       certSecretName: "wildcard-crt",
       registrySecretRefName: "harbor",
       repository: gitRepository,
