a243d0e0be8370f695987a63aab22e05da179e32
devthejo <jo@surikat.pro>
2022-08-20 23:28:26 +0200

    feat: chartPath and source

--- packages/kontinuous/src/build/load-manifests.js
+++ packages/kontinuous/src/build/load-manifests.js
@@ -1,9 +1,18 @@
 const fs = require("fs-extra")
+const set = require("lodash.set")
+
 const yaml = require("~common/utils/yaml")
 
 const removeNulls = require("~common/utils/remove-nulls")
 
+const sourcePrefix = "kontinuous-umbrella/charts/"
+
 module.exports = async (manifestsDocument, config) => {
+  manifestsDocument = manifestsDocument.replaceAll(
+    "---\n# Source: ",
+    "---\nsource: "
+  )
+
   let iterator
   try {
     iterator = yaml.loadAll(manifestsDocument)
@@ -21,8 +30,23 @@ module.exports = async (manifestsDocument, config) => {
     if (!manifest) {
       continue
     }
+    const keys = Object.keys(manifest)
+    if (keys.length === 1 && keys.includes("source")) {
+      continue
+    }
     removeNulls(manifest)
+
+    let { source } = manifest
+    if (source && source.startsWith(sourcePrefix)) {
+      source = source.slice(sourcePrefix.length)
+      const fragments = source.split("/charts/")
+      fragments.push(fragments.pop().split("/").shift())
+      const chartPath = fragments.join(".")
+      set(manifest, "metadata.annotations[kontinuous/chartPath]", chartPath)
+    }
+    set(manifest, "metadata.annotations[kontinuous/source]", source)
     manifests.push(manifest)
   }
+
   return manifests
 }
