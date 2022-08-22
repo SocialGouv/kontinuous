25dfa4956d62f07e736919402b7f726fd9ff6947
devthejo <jo@surikat.pro>
2022-08-22 14:20:39 +0200

    chore: wip

--- packages/webhook/src/config/index.js
+++ packages/webhook/src/config/index.js
@@ -1,44 +1,65 @@
 const fs = require("fs-extra")
+const yaml = require("~common/utils/yaml")
 
 const oasUri = require("./oas-uri")
 
 module.exports = async function createConfig() {
-  const secretsRootPath = "/secrets"
+  const configPath =
+    process.env.KUBEWEBHOOK_CONFIG_PATH || `${process.cwd()}/config.yaml`
+  let yamlConfig = {}
+  if (await fs.pathExists(configPath)) {
+    const yamlContent = await fs.readFile(configPath, { encoding: "utf-8" })
+    yamlConfig = yaml.load(yamlContent)
+  }
+
   const tokens = {}
   const kubeconfigs = {}
-  if (await fs.pathExists(secretsRootPath)) {
-    const secretProjectDirs = await fs.readdir(secretsRootPath)
-    for (const secretProjectDir of secretProjectDirs) {
-      const secretProjectDirPath = `${secretsRootPath}/${secretProjectDir}`
-      if (!(await fs.stat(secretProjectDirPath)).isDirectory()) {
-        continue
-      }
-      const secretTokenDir = `${secretProjectDirPath}/tokens`
-      if (!(await fs.pathExists(secretTokenDir))) {
-        continue
-      }
-      const tokenFiles = await fs.readdir(secretTokenDir)
-      tokens[secretProjectDir] = []
-      for (const tokenFile of tokenFiles) {
-        const secretTokenFile = `${secretTokenDir}/${tokenFile}/token`
-        if (!(await fs.stat(secretTokenFile)).isFile()) {
-          continue
-        }
-        const token = await fs.readFile(secretTokenFile, {
-          encoding: "utf-8",
-        })
-        tokens[secretProjectDir].push(token)
-      }
 
-      const secretKubeconfigDir = `${secretProjectDirPath}/kubeconfig`
-      const kubeconfigClusters = await fs.readdir(secretKubeconfigDir)
-      kubeconfigs[secretProjectDir] = {}
-      for (const kubeconfigCluster of kubeconfigClusters) {
-        const secretKubeconfigFile = `${secretKubeconfigDir}/${kubeconfigCluster}/kubeconfig`
-        kubeconfigs[secretProjectDir][kubeconfigCluster] = secretKubeconfigFile
-      }
-    }
+  const { projects = {} } = yamlConfig
+  for (const [projectName, projectConfig] of Object.entries(projects)) {
+    const {
+      tokens: projectTokens,
+      clusters,
+      deployKeyFile,
+      private,
+      deployKeySecretName,
+    } = projectConfig
   }
+
+  // const secretsRootPath = "/secrets"
+  // if (await fs.pathExists(secretsRootPath)) {
+  //   const secretProjectDirs = await fs.readdir(secretsRootPath)
+  //   for (const secretProjectDir of secretProjectDirs) {
+  //     const secretProjectDirPath = `${secretsRootPath}/${secretProjectDir}`
+  //     if (!(await fs.stat(secretProjectDirPath)).isDirectory()) {
+  //       continue
+  //     }
+  //     const secretTokenDir = `${secretProjectDirPath}/tokens`
+  //     if (!(await fs.pathExists(secretTokenDir))) {
+  //       continue
+  //     }
+  //     const tokenFiles = await fs.readdir(secretTokenDir)
+  //     tokens[secretProjectDir] = []
+  //     for (const tokenFile of tokenFiles) {
+  //       const secretTokenFile = `${secretTokenDir}/${tokenFile}/token`
+  //       if (!(await fs.stat(secretTokenFile)).isFile()) {
+  //         continue
+  //       }
+  //       const token = await fs.readFile(secretTokenFile, {
+  //         encoding: "utf-8",
+  //       })
+  //       tokens[secretProjectDir].push(token)
+  //     }
+
+  //     const secretKubeconfigDir = `${secretProjectDirPath}/kubeconfig`
+  //     const kubeconfigClusters = await fs.readdir(secretKubeconfigDir)
+  //     kubeconfigs[secretProjectDir] = {}
+  //     for (const kubeconfigCluster of kubeconfigClusters) {
+  //       const secretKubeconfigFile = `${secretKubeconfigDir}/${kubeconfigCluster}/kubeconfig`
+  //       kubeconfigs[secretProjectDir][kubeconfigCluster] = secretKubeconfigFile
+  //     }
+  //   }
+  // }
 
   const supertoken = process.env.KUBEWEBHOOK_SUPERTOKEN
 
