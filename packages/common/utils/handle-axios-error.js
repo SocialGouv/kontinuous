956c21442f5e7138b44ec4e29d52be2a6756a3b6
devthejo <jo@surikat.pro>
2022-08-26 16:43:42 +0200

    fix: axios error

--- packages/common/utils/handle-axios-error.js
+++ packages/common/utils/handle-axios-error.js
@@ -1,7 +1,7 @@
 const globalLogger = require("./logger")
 
 module.exports = (error, logger = globalLogger) => {
-  const url = error.request.res.responseUrl
+  const url = error.request.res?.responseUrl
   if (error.response) {
     logger.error(
       {
@@ -14,9 +14,6 @@ module.exports = (error, logger = globalLogger) => {
     if (error.response.data.msg) {
       logger.error(error.response.data.msg)
     }
-    // logger.error(error.request)
-  } else if (error.request) {
-    logger.error({ errorRequest: error.request, url }, `request error`)
   } else {
     logger.error({ errorMessage: error.message, url }, "request error")
   }
