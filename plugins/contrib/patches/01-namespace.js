db696e1597cb13844d94600ac07d3c49ce06c4ef
devthejo <jo@surikat.pro>
2022-08-30 19:10:00 +0200

    fix: overrideDefault

--- plugins/contrib/patches/01-namespace.js
+++ plugins/contrib/patches/01-namespace.js
@@ -1,11 +1,19 @@
-module.exports = (manifests, _options, { values }) => {
+const defaultOptions = {
+  overrideDefault: true,
+}
+module.exports = (manifests, options, { values }) => {
+  const opts = { ...defaultOptions, ...options }
+  const { overrideDefault } = opts
   for (const manifest of manifests) {
     const { kind, apiVersion } = manifest
     if (kind !== "Namespace" && !apiVersion.startsWith("kapp.k14s.io")) {
       if (!manifest.metadata) {
         manifest.metadata = {}
       }
-      if (!manifest.metadata.namespace) {
+      if (
+        !manifest.metadata.namespace ||
+        (overrideDefault && manifest.metadata.namespace === "default")
+      ) {
         manifest.metadata.namespace = values.global.namespace
       }
     }
