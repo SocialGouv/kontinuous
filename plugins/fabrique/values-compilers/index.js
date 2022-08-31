dec4841eddea451a0687446191f39b6c113a56e5
devthejo <jo@surikat.pro>
2022-08-31 18:04:37 +0200

    fix: ordrered with defaults before tpl

---
+++ plugins/fabrique/values-compilers/index.js
@@ -0,0 +1,13 @@
+module.exports = [
+  // "../charts/contrib/values-compilers",
+  "../charts/contrib/values-compilers/01-unfold-charts",
+  "../charts/contrib/values-compilers/02-dash-instances",
+  "../charts/contrib/values-compilers/03-resolve-alias-of",
+  "../charts/contrib/values-compilers/04-implicit-enabled",
+  "../charts/contrib/values-compilers/05-jobs",
+  "../charts/contrib/values-compilers/06-global-defaults",
+
+  "./global-defaults.js",
+
+  "../charts/contrib/values-compilers/10-tpl-meta-values",
+]
