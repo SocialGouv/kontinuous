f49dcbbd4b7ec488b8c7f5c7443bf59e8c2930a1
devthejo <jo@surikat.pro>
2022-10-06 17:33:25 +0200

    chore: wip

--- plugins/contrib/patches/05-needs-using-initcontainers.js
+++ plugins/contrib/patches/05-needs-using-initcontainers.js
@@ -1,46 +1,49 @@
-// const runKinds = ["Deployment", "StatefulSet", "DaemonSet", "Job"]
+const runKinds = ["Deployment", "StatefulSet", "DaemonSet", "Job"]
 
-module.exports = async (manifests, _options, _context) =>
-  // const { values } = context
-  // const namespace = values.global.namespace || "default"
-  // for (const manifest of manifests) {
-  // const annotations = manifest.metadata?.annotations
-  // if (!annotations) {
-  //   continue
-  // }
-  // const jsonNeeds = annotations["kontinuous/plugin.needs"]
-  // if (annotations["kontinuous/plugin.needs"]) {
-  //   delete annotations["kontinuous/plugin.needs"]
-  // }
-  // if (!runKinds.includes(manifest.kind)) {
-  //   continue
-  // }
-  // // add change-group
-  // const chartPath = annotations["kontinuous/chartPath"]
-  // if (chartPath) {
-  //   const name = chartPath.split(".").pop()
-  //   annotations[changeGroupPrefix] = `${changeGroupValuePrefix}${namespace}`
-  //   annotations[
-  //     `${changeGroupPrefix}.${name}`
-  //   ] = `${changeGroupValuePrefix}${name}.${namespace}`
-  // }
-  // // add stage if any
-  // const stage = annotations["kontinuous/plugin.stage"]
-  // if (stage) {
-  //   annotations[
-  //     `${changeGroupPrefix}.kontinuous-stage`
-  //   ] = `${changeGroupValuePrefix}/${stage}.${namespace}`
-  // }
-  // // add change-rules
-  // if (!jsonNeeds) {
-  //   continue
-  // }
-  // const needs = JSON.parse(jsonNeeds)
-  // for (const need of needs) {
-  //   annotations[
-  //     `${changeRulePrefix}.${need}`
-  //   ] = `${changeRuleValuePrefix}${need}.${namespace}`
-  // }
-  // }
+module.exports = async (manifests, _options, context) => {
+  const { values } = context
 
-  manifests
+  const namespace = values.global.namespace || "default"
+
+  const waitForGroups = {}
+
+  for (const manifest of manifests) {
+    const annotations = manifest.metadata?.annotations
+    if (!annotations) {
+      continue
+    }
+    const jsonNeeds = annotations["kontinuous/plugin.needs"]
+    if (annotations["kontinuous/plugin.needs"]) {
+      delete annotations["kontinuous/plugin.needs"]
+    }
+    if (!runKinds.includes(manifest.kind)) {
+      continue
+    }
+
+    // register wait-for chart
+    const chartPath = annotations["kontinuous/chartPath"]
+    if (chartPath) {
+      const name = chartPath.split(".").pop()
+      waitForGroups[name] = manifest
+    }
+
+    // register wait-for stage
+    const stage = annotations["kontinuous/plugin.stage"]
+    if (stage) {
+      waitForGroups[stage] = manifest
+    }
+
+    // add change-rules
+    if (!jsonNeeds) {
+      continue
+    }
+    const needs = JSON.parse(jsonNeeds)
+    for (const need of needs) {
+      annotations[
+        `${changeRulePrefix}.${need}`
+      ] = `${changeRuleValuePrefix}${need}.${namespace}`
+    }
+  }
+
+  return manifests
+}
