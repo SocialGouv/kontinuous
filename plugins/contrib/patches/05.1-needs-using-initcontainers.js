3bd689272ca3359baddf212bd969cbe688a43d73
devthejo <jo@surikat.pro>
2022-10-27 18:47:40 +0200

    fix: wip

--- plugins/contrib/patches/05.1-needs-using-initcontainers.js
+++ plugins/contrib/patches/05.1-needs-using-initcontainers.js
@@ -1,5 +1,5 @@
 const kontinuousNeedsImage =
-  "ghcr.io/socialgouv/kontinuous/wait-needs:sha-1c9866a"
+  "ghcr.io/socialgouv/kontinuous/wait-needs:sha-370feb9"
 // const kontinuousNeedsImage = "ghcr.io/socialgouv/kontinuous/wait-needs:1"
 // const kontinuousNeedsImage = "harbor.fabrique.social.gouv.fr/sre/kontinuous/wait-needs:v1"
 
