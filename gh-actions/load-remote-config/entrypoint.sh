1c674b11cb2f9700d3a723dbb14c2f02d6eac05c
devthejo <jo@surikat.pro>
2022-11-06 02:53:50 +0100

    fix: optim gh actions

--- gh-actions/load-remote-config/entrypoint.sh
+++ gh-actions/load-remote-config/entrypoint.sh
@@ -19,10 +19,10 @@ export KONTINUOUS_CONFIG=$GITHUB_WORKSPACE/kontinuous-config.json
 
 kontinuous config --remote --format json>$KONTINUOUS_CONFIG
 
-DEPLOYMENT_ID=$(get-deployment-id)
+DEPLOYMENT_NAME=$(get-deployment-name)
 
 echo "KONTINUOUS_CONFIG=$KONTINUOUS_CONFIG">>$GITHUB_ENV
-echo "DEPLOYMENT_ID=$DEPLOYMENT_ID">>$GITHUB_ENV
+echo "DEPLOYMENT_NAME=$DEPLOYMENT_NAME">>$GITHUB_ENV
 
 echo "kontinuous_config=$KONTINUOUS_CONFIG">>$GITHUB_OUTPUT
-echo "deployment_id=$DEPLOYMENT_ID">>$GITHUB_OUTPUT
+echo "deployment_name=$DEPLOYMENT_NAME">>$GITHUB_OUTPUT
