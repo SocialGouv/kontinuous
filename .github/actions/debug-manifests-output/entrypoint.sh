799e8db310ce7f96fbd3b7fc98bba96fd54473f9
devthejo <jo@surikat.pro>
2022-11-07 21:54:05 +0100

    fix: github output

--- .github/actions/debug-manifests-output/entrypoint.sh
+++ .github/actions/debug-manifests-output/entrypoint.sh
@@ -4,12 +4,10 @@ set -e
 
 export MANIFESTS="$1"
 
-JSON=$(cat "${MANIFESTS}" | npx @socialgouv/parse-manifests --json)
 MARKDOWN=$(cat "${MANIFESTS}" | npx @socialgouv/parse-manifests --markdown)
 TEXT=$(cat "${MANIFESTS}" | npx @socialgouv/parse-manifests --text)
 
 echo "${TEXT}"
 
-echo "json=\"$JSON\"" >> $GITHUB_OUTPUT
-echo "markdown=\"$MARKDOWN\"" >> $GITHUB_OUTPUT
-echo "text=\"$TEXT\"" >> $GITHUB_OUTPUT
+echo "markdown=$MARKDOWN" >> $GITHUB_OUTPUT
+echo "text=$TEXT" >> $GITHUB_OUTPUT
