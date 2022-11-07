44445843f92b024f3e0b687f796e3954664d779b
devthejo <jo@surikat.pro>
2022-11-07 23:02:53 +0100

    chore: version v1.95.38 e2e linking

--- plugins/contrib/patches/05.1-needs-using-initcontainers.js
+++ plugins/contrib/patches/05.1-needs-using-initcontainers.js
@@ -1,4 +1,4 @@
-const kontinuousNeedsImage = "ghcr.io/socialgouv/kontinuous/wait-needs:v1.95.37"
+const kontinuousNeedsImage = "ghcr.io/socialgouv/kontinuous/wait-needs:v1.95.38"
 // const kontinuousNeedsImage = "harbor.fabrique.social.gouv.fr/sre/kontinuous/wait-needs:v1"
 
 const getDeps = require("../lib/get-needs-deps")
