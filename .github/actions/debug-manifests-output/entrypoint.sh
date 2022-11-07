b083f181f3769c1b916d590613fe35d34d903741
devthejo <jo@surikat.pro>
2022-11-07 22:22:02 +0100

    fix: github output

--- .github/actions/debug-manifests-output/entrypoint.sh
+++ .github/actions/debug-manifests-output/entrypoint.sh
@@ -14,13 +14,10 @@ MARKDOWN="${MARKDOWN//$'\r'/'%0D'}"
 TEXT="${TEXT//$'\n'/'%0A'}"
 TEXT="${TEXT//$'\r'/'%0D'}"
 
-# https://github.com/orgs/community/discussions/26288#discussioncomment-3876281
-delimiter="$(openssl rand -hex 8)"
-
-echo "markdown=<<${delimiter}" >> $GITHUB_OUTPUT
+echo "markdown=<<EOF" >> $GITHUB_OUTPUT
 echo "$MARKDOWN" >> $GITHUB_OUTPUT
-echo "${delimiter}" >> $GITHUB_OUTPUT
+echo "EOF" >> $GITHUB_OUTPUT
 
-echo "text=<<${delimiter}" >> $GITHUB_OUTPUT
+echo "text=<<EOF" >> $GITHUB_OUTPUT
 echo "$TEXT" >> $GITHUB_OUTPUT
-echo "${delimiter}" >> $GITHUB_OUTPUT
+echo "EOF" >> $GITHUB_OUTPUT
