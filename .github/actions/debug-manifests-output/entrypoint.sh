ec534d69cf7fb4e805036cc2dbffc8252f672af7
devthejo <jo@surikat.pro>
2022-11-07 20:59:06 +0100

    fix: debug manifests output

--- .github/actions/debug-manifests-output/entrypoint.sh
+++ .github/actions/debug-manifests-output/entrypoint.sh
@@ -10,13 +10,6 @@ TEXT=$(cat "${MANIFESTS}" | npx @socialgouv/parse-manifests --text)
 
 echo "${TEXT}"
 
-JSON="${JSON//$'\n'/'%0A'}"
-JSON="${JSON//$'\r'/'%0D'}"
-MARKDOWN="${MARKDOWN//$'\n'/'%0A'}"
-MARKDOWN="${MARKDOWN//$'\r'/'%0D'}"
-TEXT="${TEXT//$'\n'/'%0A'}"
-TEXT="${TEXT//$'\r'/'%0D'}"
-
 echo "json=$JSON" >> $GITHUB_OUTPUT
 echo "markdown=$MARKDOWN" >> $GITHUB_OUTPUT
 echo "text=$TEXT" >> $GITHUB_OUTPUT
