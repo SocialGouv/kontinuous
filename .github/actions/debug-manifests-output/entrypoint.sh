02cb804cce90488c1162b8d29ae1c2c95b40e8a1
devthejo <jo@surikat.pro>
2022-11-07 22:40:17 +0100

    fix: debug pr

--- .github/actions/debug-manifests-output/entrypoint.sh
+++ .github/actions/debug-manifests-output/entrypoint.sh
@@ -11,9 +11,5 @@ echo "${TEXT}"
 
 MARKDOWN="${MARKDOWN//$'\n'/'%0A'}"
 MARKDOWN="${MARKDOWN//$'\r'/'%0D'}"
-TEXT="${TEXT//$'\n'/'%0A'}"
-TEXT="${TEXT//$'\r'/'%0D'}"
 
-echo "DEBUG_MANIFESTS_MARKDOWN=<<EOF" >> $GITHUB_ENV
-echo "$MARKDOWN" >> $GITHUB_ENV
-echo "EOF" >> $GITHUB_ENV
+echo "DEBUG_MANIFESTS_MARKDOWN=$MARKDOWN" >> $GITHUB_ENV
