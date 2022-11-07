45cb2a04e535300ac0cb32203d9ba39e7d072e0b
devthejo <jo@surikat.pro>
2022-11-07 19:30:04 +0100

    chore: version v1.95.17 e2e linking

--- packages/webhook/src/config/index.js
+++ packages/webhook/src/config/index.js
@@ -77,10 +77,10 @@ module.exports = async function createConfig() {
 
   const pipelineImage =
     process.env.KUBEWEBHOOK_PIPELINE_IMAGE ||
-    "ghcr.io/socialgouv/kontinuous:v1.95.16"
+    "ghcr.io/socialgouv/kontinuous:v1.95.17"
   const pipelineCheckoutImage =
     process.env.KUBEWEBHOOK_PIPELINE_CHECKOUT_IMAGE ||
-    "ghcr.io/socialgouv/kontinuous/degit:v1.95.16"
+    "ghcr.io/socialgouv/kontinuous/degit:v1.95.17"
 
   const serviceName = `kontinuous webhook server v${kontinuousVersion}`
   const config = {
