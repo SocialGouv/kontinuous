b548c681b3d38b861f0f33b9533a24031cb2cb4e
devthejo <jo@surikat.pro>
2022-11-07 22:09:12 +0100

    fix: github output

--- .github/actions/debug-manifests-output/entrypoint.sh
+++ .github/actions/debug-manifests-output/entrypoint.sh
@@ -9,5 +9,18 @@ TEXT=$(cat "${MANIFESTS}" | npx @socialgouv/parse-manifests --text)
 
 echo "${TEXT}"
 
-echo "markdown=$MARKDOWN" >> $GITHUB_OUTPUT
-echo "text=$TEXT" >> $GITHUB_OUTPUT
+MARKDOWN="${MARKDOWN//$'\n'/'%0A'}"
+MARKDOWN="${MARKDOWN//$'\r'/'%0D'}"
+TEXT="${TEXT//$'\n'/'%0A'}"
+TEXT="${TEXT//$'\r'/'%0D'}"
+
+# https://github.com/orgs/community/discussions/26288#discussioncomment-3876281
+delimiter="$(openssl rand -hex 8)"
+
+echo "markdown=<<${delimiter}" >> $GITHUB_OUTPUT
+echo "$MARKDOWN" >> $GITHUB_OUTPUT
+echo "${delimiter}" >> $GITHUB_OUTPUT
+
+echo "text=<<${delimiter}" >> $GITHUB_OUTPUT
+echo "$TEXT" >> $GITHUB_OUTPUT
+echo "${delimiter}" >> $GITHUB_OUTPUT
