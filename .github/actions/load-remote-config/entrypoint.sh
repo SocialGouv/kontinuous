3f99800ca5bbc6c0fbbd4d5f7a76cc47589030e6
devthejo <jo@surikat.pro>
2022-11-10 18:44:28 +0100

    fix: remove non pertinent deployment name

--- .github/actions/load-remote-config/entrypoint.sh
+++ .github/actions/load-remote-config/entrypoint.sh
@@ -16,13 +16,12 @@ KS_GIT_BRANCH=${KS_GIT_BRANCH#refs/tags/}
 export KS_GIT_BRANCH
 
 export KONTINUOUS_CONFIG=$GITHUB_WORKSPACE/kontinuous-config.json
-
 kontinuous config --remote --format json>$KONTINUOUS_CONFIG
-
-DEPLOYMENT_NAME=$(get-deployment-name)
-
 echo "KONTINUOUS_CONFIG=$KONTINUOUS_CONFIG">>$GITHUB_ENV
-echo "DEPLOYMENT_NAME=$DEPLOYMENT_NAME">>$GITHUB_ENV
-
 echo "kontinuous_config=$KONTINUOUS_CONFIG">>$GITHUB_OUTPUT
-echo "deployment_name=$DEPLOYMENT_NAME">>$GITHUB_OUTPUT
+
+if [ "$GITHUB_EVENT_NAME" != "delete"  ]; then
+  DEPLOYMENT_NAME=$(get-deployment-name)
+  echo "DEPLOYMENT_NAME=$DEPLOYMENT_NAME">>$GITHUB_ENV
+  echo "deployment_name=$DEPLOYMENT_NAME">>$GITHUB_OUTPUT
+fi
