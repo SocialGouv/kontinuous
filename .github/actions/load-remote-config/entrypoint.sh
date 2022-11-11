32f598b2f101183a0f731376a1acb056c5e41b8f
devthejo <jo@surikat.pro>
2022-11-11 22:53:11 +0100

    fix: deactive env

--- .github/actions/load-remote-config/entrypoint.sh
+++ .github/actions/load-remote-config/entrypoint.sh
@@ -6,6 +6,7 @@ export PATH=$PATH:/opt/kontinuous/packages/kontinuous/bin
 
 export KS_GIT=false
 export KS_GIT_REPOSITORY=$GITHUB_REPOSITORY
+
 if [ -n "$GITHUB_HEAD_REF" ]; then
   KS_GIT_BRANCH=$GITHUB_HEAD_REF
 else
@@ -13,6 +14,16 @@ else
 fi
 KS_GIT_BRANCH=${KS_GIT_BRANCH#refs/heads/}
 KS_GIT_BRANCH=${KS_GIT_BRANCH#refs/tags/}
+if [ "$GITHUB_EVENT_NAME" = "delete"  ]; then
+  KS_EVENT=deleted  
+  export EVENT_REF=$(node -e "process.stdout.write(JSON.parse(fs.readFileSync('$GITHUB_EVENT_PATH')).ref)")
+  KS_GIT_BRANCH="$EVENT_REF"
+  KS_GIT_BRANCH_URLENCODED=$(echo $KS_GIT_BRANCH | sed 's/\//\%2f/g')
+  KS_GIT_SHA="0000000000000000000000000000000000000000"
+else
+  KS_EVENT=pushed
+fi
+
 export KS_GIT_BRANCH
 
 export KONTINUOUS_CONFIG=$GITHUB_WORKSPACE/kontinuous-config.json
