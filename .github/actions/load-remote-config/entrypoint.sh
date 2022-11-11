e58626adb83919c8e72612d8dc8aff4cf1f95dd9
devthejo <jo@surikat.pro>
2022-11-11 23:03:52 +0100

    fix: deactive env

--- .github/actions/load-remote-config/entrypoint.sh
+++ .github/actions/load-remote-config/entrypoint.sh
@@ -7,24 +7,26 @@ export PATH=$PATH:/opt/kontinuous/packages/kontinuous/bin
 export KS_GIT=false
 export KS_GIT_REPOSITORY=$GITHUB_REPOSITORY
 
-if [ "$GITHUB_EVENT_NAME" = "delete"  ]; then
-  KS_GIT_BRANCH=$(node -e "process.stdout.write(JSON.parse(fs.readFileSync('$GITHUB_EVENT_PATH')).ref)")
+if [ -n "$GITHUB_HEAD_REF" ]; then
+  KS_GIT_BRANCH=$GITHUB_HEAD_REF
 else
-  if [ -n "$GITHUB_HEAD_REF" ]; then
-    KS_GIT_BRANCH=$GITHUB_HEAD_REF
-  else
-    KS_GIT_BRANCH=$GITHUB_REF
-  fi
-  KS_GIT_BRANCH=${KS_GIT_BRANCH#refs/heads/}
-  KS_GIT_BRANCH=${KS_GIT_BRANCH#refs/tags/}
+  KS_GIT_BRANCH=$GITHUB_REF
 fi
+KS_GIT_BRANCH=${KS_GIT_BRANCH#refs/heads/}
+KS_GIT_BRANCH=${KS_GIT_BRANCH#refs/tags/}
 
 export KS_GIT_BRANCH
 
 export KONTINUOUS_CONFIG=$GITHUB_WORKSPACE/kontinuous-config.json
 kontinuous config --remote --format json>$KONTINUOUS_CONFIG
+
 echo "KONTINUOUS_CONFIG=$KONTINUOUS_CONFIG">>$GITHUB_ENV
 echo "kontinuous_config=$KONTINUOUS_CONFIG">>$GITHUB_OUTPUT
+
+# set branch after load remote config, avoid trying to get config from deleted branch
+if [ "$GITHUB_EVENT_NAME" = "delete"  ]; then
+  export KS_GIT_BRANCH=$(node -e "process.stdout.write(JSON.parse(fs.readFileSync('$GITHUB_EVENT_PATH')).ref)")
+fi
 
 DEPLOYMENT_NAME=$(get-deployment-name)
 echo "DEPLOYMENT_NAME=$DEPLOYMENT_NAME">>$GITHUB_ENV
