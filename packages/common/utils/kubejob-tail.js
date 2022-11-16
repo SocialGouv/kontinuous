fe7345c5e33bda5990b08e2c60e3c997fc1c7a7d
devthejo <jo@surikat.pro>
2022-11-16 18:18:32 +0100

    feat: kubejob-tail

---
+++ packages/common/utils/kubejob-tail.js
@@ -0,0 +1,55 @@
+const kubectlRetry = require("./kubectl-retry")
+
+module.exports = async (jobName, options = {}) => {
+  const { namespace, stdout = process.stdout } = options
+
+  // to debug/test remove --follow and reswitch getLogs
+
+  const { $ } = await import("zx")
+  $.verbose = false
+
+  let lastTimestamp
+  const getLogs = async (retrying = false) => {
+    const resource = `jobs/${jobName}`
+    const logging = $`kubectl logs ${resource} ${
+      retrying ? `--since=10s` : ""
+    } --timestamps --follow`
+    for await (const chunk of logging.stdout) {
+      const lines = Buffer.from(chunk).toString()
+      for (const line of lines.split("\n")) {
+        const timeSepIndex = line.indexOf(" ")
+        if (timeSepIndex === -1) {
+          continue
+        }
+        const timestamp = line.slice(0, timeSepIndex)
+        const output = line.slice(timeSepIndex + 1)
+        if (timestamp <= lastTimestamp) {
+          continue
+        }
+        lastTimestamp = timestamp
+        stdout.write(`${output}\n`)
+      }
+    }
+
+    await logging
+  }
+
+  let ended = false
+  const maxIterations = 10
+  let countIterations = 0
+  while (!ended && countIterations <= maxIterations) {
+    await getLogs(countIterations > 0)
+    // await getLogs(true) // debug/test
+
+    const jsonStatus = await kubectlRetry(
+      `${
+        namespace ? `-n ${namespace}` : ""
+      } get job ${jobName} -o jsonpath={.status}`,
+      { logInfo: false }
+    )
+    const status = JSON.parse(jsonStatus)
+    ended =
+      !(status.active === 1) || status.succeeded === 1 || status.failed === 1
+    countIterations++
+  }
+}
