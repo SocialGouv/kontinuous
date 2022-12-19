388b03f391e3a6a26ed86236f4ad5c9a2bf82584
devthejo <jo@surikat.pro>
2022-12-19 16:32:19 +0100

    chore: wip

--- .github/actions/deploy-via-github/entrypoint.sh
+++ .github/actions/deploy-via-github/entrypoint.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-set -e
-
 mkdir -p ~/.kube
 touch ~/.kube/config
 echo $KUBECONFIG_B64 | base64 -d > ~/.kube/config
 export KUBECONFIG=~/.kube/config
+
+export KS_GIT_REPOSITORY=$GITHUB_REPOSITORY
+
+export PATH=$PATH:/opt/kontinuous/packages/kontinuous/bin
+
+export KS_BUILD_PATH=/tmp/kontinuous-deploy-via-github/
+
+kontinuous deploy
+EXIT_CODE=$?
+
+mv "$KS_BUILD_PATH/tmp/kontinuous-deploy-via-github/manifests.yaml" \
+   "$GITHUB_WORKSPACE/manifests.yaml"
+
+exit $EXIT_CODE
