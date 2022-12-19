236875ce6e7f6994d188cdccf29e3bdff5846848
devthejo <jo@surikat.pro>
2022-12-19 23:10:06 +0100

    chore: wip

--- .github/actions/deploy-via-github/entrypoint.sh
+++ .github/actions/deploy-via-github/entrypoint.sh
@@ -19,14 +19,7 @@ EXIT_CODE=$?
 mv "$KS_BUILD_PATH/manifests.yaml" \
    "$GITHUB_WORKSPACE/manifests.yaml"
 
-if [ "$EXIT_CODE" = "0" ]; then
-  DEPLOYMENT_OK="true"
-else
-  DEPLOYMENT_OK="false"
-fi
-echo "deploymentOk=$DEPLOYMENT_OK">>$GITHUB_OUTPUT
-
-if [ "$DEPLOYMENT_OK" != "true" ]; then
+if [ "$EXIT_CODE" != "0" ]; then
   echo "Pipeline failed ❌"
   exit 1
 fi
