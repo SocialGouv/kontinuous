1abdfd13a005452f7443a997f42303541ed33ffa
devthejo <jo@surikat.pro>
2022-12-19 17:32:18 +0100

    chore: wip

--- .github/actions/deploy-via-github/entrypoint.sh
+++ .github/actions/deploy-via-github/entrypoint.sh
@@ -11,6 +11,8 @@ export PATH=$PATH:/opt/kontinuous/packages/kontinuous/bin
 
 export KS_BUILD_PATH=/tmp/kontinuous-deploy-via-github/
 
+export KS_WORKSPACE_PATH=${KS_WORKSPACE_PATH:-"$GITHUB_WORKSPACE"}
+
 kontinuous deploy
 EXIT_CODE=$?
 
