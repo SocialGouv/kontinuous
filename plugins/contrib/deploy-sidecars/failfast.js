e45df0fdb7cda48a0c507ef0968addb2edf056ea
devthejo <jo@surikat.pro>
2023-01-06 00:02:39 +0100

    fix: rename rolloutStatusManifest to more explicit

--- plugins/contrib/deploy-sidecars/failfast.js
+++ plugins/contrib/deploy-sidecars/failfast.js
@@ -1,6 +1,6 @@
-const rolloutStatus = require("../lib/rollout-status")
+const rolloutStatusManifest = require("../lib/rollout-status-manifests")
 
 module.exports = async (sidecars, _options, context) => {
-  const { stop: stopSidecar, promise } = await rolloutStatus(context)
+  const { stop: stopSidecar, promise } = await rolloutStatusManifest(context)
   sidecars.push({ stopSidecar, promise })
 }
