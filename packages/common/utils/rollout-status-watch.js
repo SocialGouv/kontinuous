40862329925a8118cb0e28af9dbd184bdb5f7e38
devthejo <jo@surikat.pro>
2023-01-06 00:28:01 +0100

    feat: refactor rollout-status callers

--- packages/common/utils/rollout-status-watch.js
+++ packages/common/utils/rollout-status-watch.js
@@ -1,8 +1,7 @@
 const { setTimeout: sleep } = require("timers/promises")
 
-const rolloutStatusExec = require("./rollout-status-exec")
+const rolloutStatus = require("./rollout-status")
 const defaultLogger = require("./logger")
-const retriableOnBrokenCluster = require("./retriable-on-broken-cluster")
 
 module.exports = async ({
   namespace,
@@ -13,71 +12,28 @@ module.exports = async ({
   interceptor = {},
   rolloutStatusProcesses = {},
   checkForNewResourceInterval = 3000,
-  maxErrorRetry = 3,
-  waitBeforeRetryImagePullError = 10000,
   watchingTimeout = 3600000, // 60 minutes
   surviveOnBrokenCluster = false,
   kindFilter,
 }) => {
-  let errorRetry = 0
-
   let timeoutReached = false
   const globalTimeout = setTimeout(() => {
     timeoutReached = true
   }, watchingTimeout)
 
   while (!interceptor.stop && !timeoutReached) {
-    const { promise: rolloutStatusPromise, process: rolloutStatusProcess } =
-      rolloutStatusExec({
-        kubeconfig,
-        kubecontext,
-        namespace,
-        selector,
-        kindFilter,
-      })
-    rolloutStatusProcesses[selector] = rolloutStatusProcess
-    let status
-    try {
-      status = await rolloutStatusPromise
-      if (status.error?.code === "") {
-        throw new Error(status.error.message)
-      }
-    } catch (err) {
-      if (errorRetry >= maxErrorRetry) {
-        throw err
-      }
-      if (err.message?.includes("net/http: TLS handshake timeout")) {
-        logger.debug(
-          { namespace, selector },
-          `rollout-status network error(net/http: TLS handshake timeout): retrying...`
-        )
-        await sleep(3000)
-        errorRetry++
-        continue
-      }
-      if (surviveOnBrokenCluster) {
-        const retriable = retriableOnBrokenCluster(err)
-        if (retriable.retry) {
-          logger.debug(
-            { error: err, from: "rollout-status" },
-            `${retriable.message}, retrying...`
-          )
-          await sleep(3000)
-          errorRetry++
-          continue
-        }
-      }
-      if (err.message?.includes("ErrImagePull")) {
-        logger.debug(
-          { namespace, selector },
-          `rollout-status registry error(ErrImagePull): retrying...`
-        )
-        await sleep(waitBeforeRetryImagePullError)
-        errorRetry++
-        continue
-      }
-      throw err
-    }
+    const status = await rolloutStatus({
+      kubeconfig,
+      kubecontext,
+      namespace,
+      selector,
+      kindFilter,
+      surviveOnBrokenCluster,
+      setProcessRef: (rolloutStatusProcess) => {
+        rolloutStatusProcesses[selector] = rolloutStatusProcess
+      },
+      logger,
+    })
     const { success, error } = status
     if (success || error.code !== "not-found") {
       clearTimeout(globalTimeout)
--- packages/common/utils/rollout-status.js
+++ packages/common/utils/rollout-status.js
@@ -1,38 +1,76 @@
 const retry = require("async-retry")
 
 const rolloutStatusExec = require("./rollout-status-exec")
+const retriableOnBrokenCluster = require("./retriable-on-broken-cluster")
 
-const globalLogger = require("./logger")
+const defaultLogger = require("./logger")
 
 module.exports = async ({
   kubeconfig,
   kubecontext,
   namespace,
   selector,
-  logger = globalLogger,
+  logger = defaultLogger,
   ignoreSecretNotFound = true,
-}) =>
-  retry(
+  retryErrImagePull = true,
+  setProcessRef,
+  kindFilter,
+  surviveOnBrokenCluster,
+}) => {
+  const throwRetriableError = async (err, status) => {
+    if (err.message?.includes("net/http: TLS handshake timeout")) {
+      logger.debug(
+        { namespace, selector },
+        `rollout-status network error(net/http: TLS handshake timeout): retrying...`
+      )
+      throw err
+    }
+    if (retryErrImagePull && err.message?.includes("ErrImagePull")) {
+      logger.debug(
+        { namespace, selector },
+        `rollout-status registry error(ErrImagePull): retrying...`
+      )
+      throw err
+    }
+    if (surviveOnBrokenCluster) {
+      const retriable = retriableOnBrokenCluster(err)
+      if (retriable.retry) {
+        logger.debug(
+          { error: err, from: "rollout-status" },
+          `${retriable.message}, retrying...`
+        )
+        throw err
+      }
+    }
+    if (status?.type === "program") {
+      throw new Error(status.error.message)
+    }
+  }
+
+  return retry(
     async (bail) => {
+      let status
       try {
-        const { promise } = rolloutStatusExec({
+        const { promise, process } = rolloutStatusExec({
           kubeconfig,
           kubecontext,
           namespace,
           selector,
           ignoreSecretNotFound,
+          kindFilter,
         })
-        const output = await promise
-        return output
-      } catch (err) {
-        if (err.message.includes("net/http: TLS handshake timeout")) {
-          logger.debug(
-            `rollout-status network error(net/http: TLS handshake timeout): retrying...`
-          )
-          throw err
+        if (setProcessRef) {
+          setProcessRef(process)
         }
+        status = await promise
+        if (status.error?.code === "") {
+          throw new Error(status.error.message)
+        }
+      } catch (err) {
+        throwRetriableError(err, status)
         bail(err)
       }
+      return status
     },
     {
       retries: 2,
@@ -41,3 +79,4 @@ module.exports = async ({
       maxTimeout: 3000,
     }
   )
+}
