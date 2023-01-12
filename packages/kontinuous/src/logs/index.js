7f1c383e3701e5e5a1f884eb3727716ee4242712
devthejo <jo@surikat.pro>
2023-01-12 19:32:02 +0100

    fix: webhook logs increase retries

--- packages/kontinuous/src/logs/index.js
+++ packages/kontinuous/src/logs/index.js
@@ -131,10 +131,11 @@ module.exports = async (options) => {
         }
       },
       {
-        retries: 3,
-        factor: 1,
+        retries: 10,
+        factor: 2,
         minTimeout: 1000,
-        maxTimeout: 3000,
+        maxTimeout: 60000,
+        randomize: true,
       }
     )
   } catch (error) {
