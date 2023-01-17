ce7018857f559038e768997303f7624d76425f70
devthejo <jo@surikat.pro>
2023-01-17 11:29:50 +0100

    fix: deactivate wip

--- .github/actions/deploy-via-github/entrypoint.sh
+++ .github/actions/deploy-via-github/entrypoint.sh
@@ -13,11 +13,11 @@ export KS_BUILD_PATH=/tmp/kontinuous-deploy-via-github
 
 export KS_WORKSPACE_PATH=${KS_WORKSPACE_PATH:-"$GITHUB_WORKSPACE"}
 
-GH_EVENT_JSON_CONFIG=$(node -e "process.stdout.write(fs.readFileSync('$GITHUB_EVENT_PATH',{encoding:'utf-8'}))")
 if [ "$GITHUB_EVENT_NAME" = "delete"  ]; then
   export KS_EVENT=deleted
   export EVENT_REF=$(node -e "process.stdout.write(($GH_EVENT_JSON_CONFIG).ref || '')")
-  export KS_GIT_BRANCH="$EVENT_REF"
+  GH_EVENT_JSON_CONFIG=$(node -e "process.stdout.write(fs.readFileSync('$GITHUB_EVENT_PATH',{encoding:'utf-8'}))")
+  export KS_GIT_BRANCH=$(node -e "const githubEvent = $GH_EVENT_JSON_CONFIG;const ref = githubEvent.ref || githubEvent.pull_request?.head?.ref || '' ;process.stdout.write(ref);")
   export KS_GIT_SHA="0000000000000000000000000000000000000000"
 else
   export KS_EVENT=pushed
