c867117b8213d808a6de044c6c45f205aaf39586
devthejo <jo@surikat.pro>
2023-01-26 17:52:19 +0100

    chore: test

--- .github/scripts/test-deploy/prepare-kontinuous.sh
+++ .github/scripts/test-deploy/prepare-kontinuous.sh
@@ -9,27 +9,6 @@ links:
 
 echo "KUBECONFIG_B64=$(cat ~/.kube/config | base64 -w 0)" >> $GITHUB_ENV
 
-
-
-echo "
-kind: Cluster
-apiVersion: kind.x-k8s.io/v1alpha4
-networking:
-  apiServerAddress: '127.0.0.1'
-  apiServerPort: 6443
-" > $HOME/kind-config.yaml
-
-kind create cluster \
-  --kubeconfig $KUBECONFIG \
-  --config=$HOME/kind-config.yaml
-
-kubectl config set-cluster kind-kind --server=https://kind-control-plane:6443 
-
-
-
-
-docker network connect kind $(cat /etc/hostname)
-
 kubectl cluster-info
 kubectl version
 kubectl get pods -n kube-system
