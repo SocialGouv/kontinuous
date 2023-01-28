4cc77585551884f78e62228087a1927cffad37b3
devthejo <jo@surikat.pro>
2023-01-28 10:24:41 +0100

    feat: implement renovate in js

--- packages/common/versions.js
+++ packages/common/versions.js
@@ -1,7 +1,16 @@
 module.exports = {
+  // renovate: datasource=github-releases depName=helm/helm
   helm: "v3.11.0",
+
+  // renovate: datasource=github-tags depName=kubectl lookupName=kubernetes/kubectl extractVersion=^kubernetes-(?<version>.+)$
   kubectl: "v1.26.1",
+
+  // renovate: datasource=github-releases depName=socialgouv/rollout-status
   rolloutStatus: "v1.13.4",
+
+  // renovate: datasource=github-releases depName=stern/stern extractVersion=^v(?<version>.+)$
   stern: "1.22.0",
+
+  // renovate: datasource=github-releases depName=carvel-dev/kapp
   kapp: "v0.52.0",
 }
