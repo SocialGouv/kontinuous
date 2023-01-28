c45e05c64f65a5659d3aeab5c74d1537b30cbd32
devthejo <jo@surikat.pro>
2023-01-28 17:32:44 +0100

    chore: renovate wip

--- packages/common/versions.js
+++ packages/common/versions.js
@@ -2,7 +2,7 @@ module.exports = {
   // renovate: datasource=github-releases depName=helm/helm
   helm: "3.11.0",
 
-  // renovate: datasource=github-tags depName=kubectl lookupName=kubernetes/kubectl extractVersion=^kubernetes-(?<version>.+)$
+  // renovate: datasource=github-tags depName=kubernetes/kubectl extractVersion=^kubernetes-(?<version>.+)$
   kubectl: "1.26.1",
 
   // renovate: datasource=github-releases depName=socialgouv/rollout-status
