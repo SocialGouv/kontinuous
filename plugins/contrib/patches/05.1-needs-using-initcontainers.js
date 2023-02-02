8e85a88928f100506295ad47a276524d7d893f33
devthejo <jo@surikat.pro>
2023-02-02 22:03:08 +0100

    chore(release): 1.133.9

--- plugins/contrib/patches/05.1-needs-using-initcontainers.js
+++ plugins/contrib/patches/05.1-needs-using-initcontainers.js
@@ -1,4 +1,4 @@
-const kontinuousNeedsImage = "ghcr.io/socialgouv/kontinuous/wait-needs:v1.133.8"
+const kontinuousNeedsImage = "ghcr.io/socialgouv/kontinuous/wait-needs:v1.133.9"
 // const kontinuousNeedsImage = "harbor.fabrique.social.gouv.fr/sre/kontinuous/wait-needs:v1"
 
 const getDeps = require("../lib/get-needs-deps")
