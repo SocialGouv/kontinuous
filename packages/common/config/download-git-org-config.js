e0b5d770742f323f89b1b34d623b9fa3e846576e
Jo <jo@surikat.pro>
2023-02-06 10:51:10 +0100

    feat: add git org path param (#189)

--- packages/common/config/download-git-org-config.js
+++ packages/common/config/download-git-org-config.js
@@ -4,11 +4,11 @@ const getRepositoryFile = require("~common/utils/get-repository-file")
 
 const getLogger = require("~common/utils/get-logger")
 
-module.exports = async (repositoryUrl, ref, logger = getLogger()) => {
+module.exports = async (repositoryUrl, file, ref, logger = getLogger()) => {
   const data = await getRepositoryFile({
     repositoryUrl,
     ref,
-    file: "config.yaml",
+    file,
     logger,
   })
   return data ? yaml.load(data) : {}
