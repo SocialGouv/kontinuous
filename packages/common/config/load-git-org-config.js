588174f927706bcd9b18a5e9085ebe19e3267a34
devthejo <jo@surikat.pro>
2023-02-14 14:22:18 +0100

    fix: git-org

--- packages/common/config/load-git-org-config.js
+++ packages/common/config/load-git-org-config.js
@@ -1,6 +1,5 @@
 const defaultsDeep = require("lodash.defaultsdeep")
 
-const getGitUrl = require("~common/utils/get-git-url")
 const normalizeRepositoryUrl = require("~common/utils/normalize-repository-url")
 
 const getGitRemoteDefaultBranch = require("~common/utils/get-git-remote-default-branch")
@@ -15,37 +14,41 @@ module.exports = async (config, reloadConfig) => {
     gitOrgPath,
     gitOrgRequired,
     gitOrgRef,
+    gitRepositoryUrl,
   } = config
   if (!gitOrg) {
     return config
   }
 
-  let gitUrl
-  try {
-    gitUrl = await getGitUrl()
-  } catch (error) {
+  if (!gitRepositoryUrl) {
     if (gitOrgRequired) {
-      throw error
+      throw new Error(
+        "unable to determine gitRepositoryUrl and gitOrgRequired is set to true"
+      )
     }
     return config
   }
 
   let orga
+  let host
+  let protocol
   if (gitOrgOverride) {
     orga = gitOrgOverride
   } else {
-    const url = parseGitUrl(gitUrl)
+    const url = parseGitUrl(gitRepositoryUrl)
     orga = url.owner
+    protocol = url.protocol
+    host = url.host
   }
 
-  const gitRepository = `${orga}/${gitOrgRepository}`
+  const gitRepository = `${protocol}://${host}/${orga}/${gitOrgRepository}`
 
-  const gitRepositoryUrl = normalizeRepositoryUrl(gitRepository, "ssh")
+  const gitOrgRepositoryUrl = normalizeRepositoryUrl(gitRepository, "ssh")
 
   let exists
   let defaultRef
   try {
-    defaultRef = await getGitRemoteDefaultBranch(gitRepositoryUrl)
+    defaultRef = await getGitRemoteDefaultBranch(gitOrgRepositoryUrl)
     exists = true
   } catch (error) {
     if (gitOrgRequired) {
@@ -61,7 +64,7 @@ module.exports = async (config, reloadConfig) => {
   const ref = gitOrgRef || defaultRef
 
   const orgaConfig = await downloadGitOrgConfig(
-    gitRepositoryUrl,
+    gitOrgRepositoryUrl,
     gitOrgPath,
     ref
   )
