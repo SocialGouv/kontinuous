ee39055df7f436835ab57f7d64f7fbc13aae2718
devthejo <jo@surikat.pro>
2023-02-28 12:49:49 +0100

    Revert "chore: hide release commits from diff"
    This reverts commit 4e90627b2bc56d3b99a43c782b1e39a34ea27801.

--- plugins/contrib/patches/05.1-needs-using-initcontainers.js
+++ plugins/contrib/patches/05.1-needs-using-initcontainers.js
@@ -1,4 +1,4 @@
-const kontinuousNeedsImage = "ghcr.io/socialgouv/kontinuous/wait-needs:v1.134.0"
+const kontinuousNeedsImage = "ghcr.io/socialgouv/kontinuous/wait-needs:v1.134.3"
 // const kontinuousNeedsImage = "harbor.fabrique.social.gouv.fr/sre/kontinuous/wait-needs:v1"
 
 const getDeps = require("../lib/get-needs-deps")
