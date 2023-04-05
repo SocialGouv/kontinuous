2208bc8d9baae36b3aedd4169af575b3e2372c46
devthejo <jo@surikat.pro>
2023-04-05 08:14:56 +0200

    fix: forceRestart optout missing keys

--- plugins/contrib/patches/00.3-force-restart-optout.js
+++ plugins/contrib/patches/00.3-force-restart-optout.js
@@ -1,16 +1,24 @@
 module.exports = (manifests, _options, { config }) => {
-  const { deploymentLabelKey } = config
+  const { deploymentLabelKey, refLabelKey } = config
 
+  const cleanKeys = [
+    deploymentLabelKey,
+    refLabelKey,
+    "kontinuous/gitSha",
+    "kontinuous/appVersion",
+  ]
   for (const manifest of manifests) {
     if (
       manifest.metadata?.annotations?.["kontinuous/plugin.forceRestart"] ===
       "false"
     ) {
-      if (manifest.spec.template?.metadata?.annotations?.[deploymentLabelKey]) {
-        delete manifest.spec.template.metadata.annotations[deploymentLabelKey]
-      }
-      if (manifest.spec.template?.metadata?.labels?.[deploymentLabelKey]) {
-        delete manifest.spec.template.metadata.labels[deploymentLabelKey]
+      for (const key of cleanKeys) {
+        if (manifest.spec.template?.metadata?.annotations?.[key]) {
+          delete manifest.spec.template.metadata.annotations[key]
+        }
+        if (manifest.spec.template?.metadata?.labels?.[key]) {
+          delete manifest.spec.template.metadata.labels[key]
+        }
       }
     }
   }
