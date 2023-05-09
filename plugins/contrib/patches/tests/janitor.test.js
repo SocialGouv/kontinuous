9f1b08a8942abe8c2cb92bba5ae286112bd3f970
devthejo <jo@surikat.pro>
2023-05-09 10:05:51 +0200

    fix: plugins tests

--- plugins/contrib/patches/tests/janitor.test.js
+++ plugins/contrib/patches/tests/janitor.test.js
@@ -7,7 +7,7 @@ metadata:
   name: some-ns
 `
 
-const runJanitor = (config, values) => {
+const runJanitor = async (config, values) => {
   const ctx = require("~common/ctx")
   const utils = require("~common/utils")
 
@@ -30,12 +30,15 @@ const runJanitor = (config, values) => {
 }
 
 test(`add janitor annotation in dev`, async () => {
-  const result = runJanitor({ environment: "dev", gitBranch: "test-branch" })
+  const result = await runJanitor({
+    environment: "dev",
+    gitBranch: "test-branch",
+  })
   expect(result[0].metadata.annotations["janitor/ttl"]).toEqual("7d")
 })
 
 test(`add custom janitor annotation in dev`, async () => {
-  const result = runJanitor(
+  const result = await runJanitor(
     { environment: "dev", gitBranch: "test-branch" },
     { ttl: "24h" }
   )
@@ -43,16 +46,19 @@ test(`add custom janitor annotation in dev`, async () => {
 })
 
 test(`DONT add janitor in persist env`, async () => {
-  const result = runJanitor({ environment: "dev", gitBranch: "test/persist" })
+  const result = await runJanitor({
+    environment: "dev",
+    gitBranch: "test/persist",
+  })
   expect(() => result[0].metadata.annotations["janitor/ttl"]).toThrow()
 })
 
 test(`DONT add janitor annotation in preprod`, async () => {
-  const result = runJanitor({ environment: "preprod" })
+  const result = await runJanitor({ environment: "preprod" })
   expect(() => result[0].metadata.annotations["janitor/ttl"]).toThrow()
 })
 
 test(`DONT add janitor annotation in prod`, async () => {
-  const result = runJanitor({ environment: "prod" })
+  const result = await runJanitor({ environment: "prod" })
   expect(() => result[0].metadata.annotations["janitor/ttl"]).toThrow()
 })
