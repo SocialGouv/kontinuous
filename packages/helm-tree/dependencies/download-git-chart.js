c9a57a40bf03797946a9a797ef790748eb0824b7
devthejo <jo@surikat.pro>
2023-05-11 13:18:09 +0200

    chore: test

--- packages/helm-tree/dependencies/download-git-chart.js
+++ packages/helm-tree/dependencies/download-git-chart.js
@@ -1,6 +1,6 @@
 const degitImproved = require("~common/utils/degit-improved")
 
-const normalizeDegitUri = require("~common/utils/normalize-degit-uri")
+// const normalizeDegitUri = require("~common/utils/normalize-degit-uri")
 
 const slug = require("~common/utils/slug")
 const matchLinkRemap = require("~common/utils/match-link-remap")
@@ -13,7 +13,7 @@ module.exports = async ({ dependency, cachePath, logger }) => {
   let { degit: degitUri } = dependency
   degitUri = degitUri.replaceAll("@", "#")
 
-  degitUri = normalizeDegitUri(degitUri)
+  // degitUri = normalizeDegitUri(degitUri)
 
   const { links = {}, remoteLinks = {} } = config
 
