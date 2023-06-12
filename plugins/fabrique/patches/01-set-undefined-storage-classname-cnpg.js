0d2f99f3fe909f0a8e3b091ab0be589ef55ae889
devthejo <jo@surikat.pro>
2023-06-12 13:36:14 +0200

    fix: undefined storage

--- plugins/fabrique/patches/01-set-undefined-storage-classname-cnpg.js
+++ plugins/fabrique/patches/01-set-undefined-storage-classname-cnpg.js
@@ -6,12 +6,14 @@ module.exports = (manifests, options) => {
   const clusterManifests = manifests.filter((m) => m.kind === "Cluster")
   const undefinedStorageClassManifests = clusterManifests.filter(
     (m) =>
-      m.spec.storage &&
-      (m.spec.storage?.storageClass === undefined ||
-        m.spec.storage?.storageClass === null ||
-        m.spec.storage?.storageClass === "")
+      m.spec.storage?.storageClass === undefined ||
+      m.spec.storage?.storageClass === null ||
+      m.spec.storage?.storageClass === ""
   )
   for (const manifest of undefinedStorageClassManifests) {
+    if (!manifest.spec.storage) {
+      manifest.spec.storage = {}
+    }
     manifest.spec.storage.storageClass = defaultStorageClassName
   }
 }
