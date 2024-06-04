711e5c1e97149de76410f2cef8dc9537b9acb3e2
devthejo <devthejo@protonmail.com>
2024-06-04 09:37:26 +0200

    fix: improve syntax

--- plugins/contrib/patches/40-add-jobs-affinity-and-tolerations.js
+++ plugins/contrib/patches/40-add-jobs-affinity-and-tolerations.js
@@ -12,16 +12,14 @@ module.exports = (manifests, options) => {
 
     const parentSpec = kind === "CronJob" ? manifest.spec.jobTemplate : manifest
 
-    parentSpec.spec.template.spec = parentSpec.spec.template.spec || {}
+    parentSpec.spec.template.spec ||= {}
     const templateSpec = parentSpec.spec.template.spec
 
     // Add or merge affinity
-    templateSpec.affinity = templateSpec.affinity || {}
-    templateSpec.affinity.nodeAffinity =
-      templateSpec.affinity.nodeAffinity || {}
-    templateSpec.affinity.nodeAffinity.preferredDuringSchedulingIgnoredDuringExecution =
-      templateSpec.affinity.nodeAffinity
-        .preferredDuringSchedulingIgnoredDuringExecution || []
+    templateSpec.affinity ||= {}
+    templateSpec.affinity.nodeAffinity ||= {}
+    templateSpec.affinity.nodeAffinity.preferredDuringSchedulingIgnoredDuringExecution ||=
+      []
     const existingPreferences =
       templateSpec.affinity.nodeAffinity
         .preferredDuringSchedulingIgnoredDuringExecution
@@ -38,7 +36,7 @@ module.exports = (manifests, options) => {
     }
 
     // Add or merge tolerations
-    templateSpec.tolerations = templateSpec.tolerations || []
+    templateSpec.tolerations ||= []
     const newTolerations = tolerationsToAdd.filter(
       (tolerationToAdd) =>
         !templateSpec.tolerations.some(
