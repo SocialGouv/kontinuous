d1b904d7dd490e35c1c1a44e8541d3800c8e1828
Your Name <you@example.com>
2024-09-19 15:04:28 +0200

    chore: add call to supabase

--- plugins/kontinuous-ui/pre-deploy/01-deployment_event.js
+++ plugins/kontinuous-ui/pre-deploy/01-deployment_event.js
@@ -1,19 +1,32 @@
-// const { createClient } = require("@supabase/supabase-js")
+const { createClient } = require("@supabase/supabase-js")
 
-module.exports = async (manifests, _options, { config, logger }) => {
-  const { repositoryName } = config
-  logger.debug("*****> REPOSITORY NAME:", repositoryName)
+module.exports = async (_manifests, _options, { config }) => {
+  const {
+    action,
+    gitSha,
+    projectName,
+    environment,
+    refLabelValue,
+    repositoryName,
+  } = config
 
-  // const supabase = createClient(
-  //   "https://dabwsunwcukreynmegja.supabase.co",
-  //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhYndzdW53Y3VrcmV5bm1lZ2phIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU2NDQwMDksImV4cCI6MjA0MTIyMDAwOX0.8779ZywO5dvRLfMGMShlxBUqGeqC6WK4k3e0puBN7So"
-  // )
-  // logger.info("Creation du client supabase")
-  // const { data, error } = await supabase.from("users").select("*")
-  // logger.info("Execution de la requete", error)
-  // if (error) throw error
-  // logger.info("DATA:", JSON.stringify(data, null, 2))
-  logger.info("CONFIG", config)
+  const supabase = createClient(
+    "https://dabwsunwcukreynmegja.supabase.co",
+    "XXXX"
+  )
+
+  const { error } = await supabase.from("deployments_logs").insert([
+    {
+      status: action,
+      commit_hash: gitSha,
+      project: projectName,
+      environment,
+      branch: refLabelValue,
+      repository: repositoryName,
+    },
+  ])
+
+  if (error) throw error
 }
 
 // ../kontinuous/kontinuous deploy --kubeconfig-context ovh-dev --dry-run -d
