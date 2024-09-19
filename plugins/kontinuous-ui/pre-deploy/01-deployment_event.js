31e5218ac1d52ac604f21f9b5b49f51dc62352f3
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2024-09-19 13:25:17 +0200

    feat: catch execution events

---
+++ plugins/kontinuous-ui/pre-deploy/01-deployment_event.js
@@ -0,0 +1,17 @@
+// const { createClient } = require("@supabase/supabase-js")
+
+module.exports = async (manifests, _options, { config, logger }) => {
+  const { repositoryName } = config
+  logger.debug("*****> REPOSITORY NAME:", repositoryName)
+
+  // const supabase = createClient(
+  //   "https://dabwsunwcukreynmegja.supabase.co",
+  //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhYndzdW53Y3VrcmV5bm1lZ2phIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU2NDQwMDksImV4cCI6MjA0MTIyMDAwOX0.8779ZywO5dvRLfMGMShlxBUqGeqC6WK4k3e0puBN7So"
+  // )
+  // logger.info("Creation du client supabase")
+  // const { data, error } = await supabase.from("users").select("*")
+  // logger.info("Execution de la requete", error)
+  // if (error) throw error
+  // logger.info("DATA:", JSON.stringify(data, null, 2))
+  logger.info("CONFIG", config)
+}
