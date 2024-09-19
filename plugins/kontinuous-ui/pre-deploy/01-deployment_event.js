4aa5777e7609d8be9860dc704fb3716f89854a8b
Your Name <you@example.com>
2024-09-19 15:10:13 +0200

    chore: remove comments

--- plugins/kontinuous-ui/pre-deploy/01-deployment_event.js
+++ plugins/kontinuous-ui/pre-deploy/01-deployment_event.js
@@ -12,7 +12,7 @@ module.exports = async (_manifests, _options, { config }) => {
 
   const supabase = createClient(
     "https://dabwsunwcukreynmegja.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhYndzdW53Y3VrcmV5bm1lZ2phIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU2NDQwMDksImV4cCI6MjA0MTIyMDAwOX0.8779ZywO5dvRLfMGMShlxBUqGeqC6WK4k3e0puBN7So"
+    "XXXX"
   )
 
   const { error } = await supabase.from("deployments_logs").insert([
@@ -28,5 +28,3 @@ module.exports = async (_manifests, _options, { config }) => {
 
   if (error) throw error
 }
-
-// ../kontinuous/kontinuous deploy --kubeconfig-context ovh-dev --dry-run -d
