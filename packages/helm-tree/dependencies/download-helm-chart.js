1437fd879d92b41202d118d7a67cd1c6081c8582
devthejo <devthejo@protonmail.com>
2024-11-05 09:49:43 +0100

    feat: handle semver dependencies

--- packages/helm-tree/dependencies/download-helm-chart.js
+++ packages/helm-tree/dependencies/download-helm-chart.js
@@ -1,10 +1,20 @@
 const fs = require("fs-extra")
-
+const semver = require("semver")
 const axios = require("~common/utils/axios-retry")
 const yaml = require("~common/utils/yaml")
 const downloadFile = require("~common/utils/download-file")
 const slug = require("~common/utils/slug")
 const handleAxiosError = require("~common/utils/handle-axios-error")
+
+function satisfiesVersion(availableVersion, requiredVersion) {
+  // If requiredVersion is an exact version (no special characters), do a direct comparison
+  if (/^\d+\.\d+\.\d+$/.test(requiredVersion)) {
+    return availableVersion === requiredVersion
+  }
+
+  // For all other cases, use semver.satisfies
+  return semver.satisfies(availableVersion, requiredVersion)
+}
 
 module.exports = async ({ dependency, target, cachePath, logger }) => {
   const { repository, version } = dependency
@@ -31,13 +41,19 @@ module.exports = async ({ dependency, target, cachePath, logger }) => {
       const repo = yaml.load(repositoryIndex.data)
       const { entries } = repo
       const entryVersions = entries[dependency.name]
-      const versionEntry = entryVersions.find(
-        (entry) => entry.version.toString() === dependency.version.toString()
+      const versionEntry = entryVersions.find((entry) =>
+        satisfiesVersion(entry.version, dependency.version)
       )
       if (!versionEntry) {
-        throw new Error(`version ${version} not found for ${dependency.name}`)
+        throw new Error(
+          `No matching version found for ${dependency.name}@${version}`
+        )
       }
-      const url = versionEntry.urls[0]
+      let url = versionEntry.urls[0]
+      // Check if the URL is relative and add the repository as prefix if needed
+      if (!url.startsWith("http://") && !url.startsWith("https://")) {
+        url = `${repository.replace(/\/$/, "")}/${url.replace(/^\//, "")}`
+      }
       logger.debug(`⬇️  downloading chart ${url}`)
       await downloadFile(url, zfile, logger)
     }
