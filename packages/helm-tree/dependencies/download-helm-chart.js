c344213d1a3455730d4c90412e1a9f0378185b3a
Jo <jo@surikat.pro>
2024-11-05 18:46:59 +0100

    feat: handle semver dependencies (#515)

--- packages/helm-tree/dependencies/download-helm-chart.js
+++ packages/helm-tree/dependencies/download-helm-chart.js
@@ -1,10 +1,20 @@
 const fs = require("fs-extra")
-
+const semver = require("semver")
 const axios = require("~common/utils/axios-retry")
 const yaml = require("~common/utils/yaml")
 const downloadFile = require("~common/utils/download-file")
 const slug = require("~common/utils/slug")
 const handleAxiosError = require("~common/utils/handle-axios-error")
+
+function satisfiesVersion(availableVersion, requiredVersion) {
+  // If requiredVersion is an exact version (no special characters), do a direct comparison
+  if (/^\d+\.\d+\.\d+$/.test(requiredVersion)) {
+    return availableVersion === requiredVersion
+  }
+
+  // For all other cases, use semver.satisfies
+  return semver.satisfies(availableVersion, requiredVersion)
+}
 
 module.exports = async ({ dependency, target, cachePath, logger }) => {
   const { repository, version } = dependency
@@ -31,13 +41,29 @@ module.exports = async ({ dependency, target, cachePath, logger }) => {
       const repo = yaml.load(repositoryIndex.data)
       const { entries } = repo
       const entryVersions = entries[dependency.name]
-      const versionEntry = entryVersions.find(
-        (entry) => entry.version.toString() === dependency.version.toString()
+
+      // Find all versions that satisfy the constraint
+      const satisfyingVersions = entryVersions.filter((entry) =>
+        satisfiesVersion(entry.version, dependency.version)
       )
-      if (!versionEntry) {
-        throw new Error(`version ${version} not found for ${dependency.name}`)
+
+      if (satisfyingVersions.length === 0) {
+        throw new Error(
+          `No matching version found for ${dependency.name}@${version}`
+        )
       }
-      const url = versionEntry.urls[0]
+
+      // Sort the satisfying versions in descending order
+      satisfyingVersions.sort((a, b) => semver.rcompare(a.version, b.version))
+
+      // Select the newest version that satisfies the constraint
+      const versionEntry = satisfyingVersions[0]
+
+      let url = versionEntry.urls[0]
+      // Check if the URL is relative and add the repository as prefix if needed
+      if (!url.startsWith("http://") && !url.startsWith("https://")) {
+        url = `${repository.replace(/\/$/, "")}/${url.replace(/^\//, "")}`
+      }
       logger.debug(`⬇️  downloading chart ${url}`)
       await downloadFile(url, zfile, logger)
     }
