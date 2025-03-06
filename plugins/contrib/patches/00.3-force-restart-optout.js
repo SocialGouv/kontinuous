0e9498c85c9c27477781775dde19c82d8a3f327e
devthejo <devthejo@protonmail.com>
2025-03-06 09:30:50 +0100

    fix: forceRestart optout typo

--- plugins/contrib/patches/00.3-force-restart-optout.js
+++ plugins/contrib/patches/00.3-force-restart-optout.js
@@ -1,6 +1,5 @@
 module.exports = (manifests, _options, { config }) => {
   const { deploymentLabelKey, refLabelKey } = config
-
   const cleanKeys = [
     deploymentLabelKey,
     refLabelKey,
@@ -10,12 +9,10 @@ module.exports = (manifests, _options, { config }) => {
   for (const manifest of manifests) {
     if (
       manifest.metadata?.annotations?.["kontinuous/plugin.forceRestart"] ===
-      "false"
+        "false" &&
+      manifest.spec
     ) {
       for (const key of cleanKeys) {
-        if (!manifest.spec) {
-          return
-        }
         if (manifest.spec.template?.metadata?.annotations?.[key]) {
           delete manifest.spec.template.metadata.annotations[key]
         }
